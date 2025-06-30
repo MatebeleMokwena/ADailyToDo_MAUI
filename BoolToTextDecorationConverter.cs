edc564bea6b5e66ab04f8f56bf8fd0a048b10a3a
MatebeleMokwena <142404003+MatebeleMokwena@users.noreply.github.com>
2025-06-30 12:00:32 +0200

    Add files via upload
    Converter for the isComplete function

---
+++ BoolToTextDecorationConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDailyToDo.TheToDo
+{
+    public class BoolToTextDecorationConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if ((value != null))
+            {
+                bool b = (bool)value;
+                if (b) { return TextDecorations.Strikethrough; }
+                return TextDecorations.None;
+            }
+            return TextDecorations.None;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
